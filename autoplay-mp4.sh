dcc47273cda799a75b5497164094ba0a3a3818e9
Chris <electronicsleep@gmail.com>
2016-03-15 22:12:11 -0700

    MediaPi Movie Player

--- autoplay-mp4.sh
+++ autoplay-mp4.sh
@@ -5,7 +5,7 @@
 # Purpose: Movie Looper for the Rasberry Pi
 # Released under the BSD license
 
-# Create playlist file used for starting moive play and run script
+# Create playlist file used for starting movie play and run script
 
 #echo "featured" > /tmp/play-mp4.txt
 #bash -x /media/FLASHDEVICE/play-mp4.sh
