7048dca102515d5f84830f7191a518380e4c691b
Chris <electronicsleep@gmail.com>
2016-11-14 23:26:11 -0800

    adding crontab note to script

--- autoplay-mp4.sh
+++ autoplay-mp4.sh
@@ -5,6 +5,10 @@
 # Purpose: Movie Looper for the Rasberry Pi
 # Released under the BSD license
 
+#MEDIAPI AUTOPLAY | crontab -e
+#*/15 * * * *  bash -x /media/FLASHDEVICE/autoplay-mp4.sh > /media/FLASHDEVICE/autoplay-mp4.out 
+#@reboot  bash -x /media/FLASHDEVICE/autoplay-mp4.sh > /media/FLASHDEVICE/autoplay-mp4.out 
+
 # send playlist file used for starting movie loop play and run play-mp4 script
 
 echo "featured" > /tmp/play-mp4.txt
